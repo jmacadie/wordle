ff942270bea7bc7f62688b0684501b25fd500119
James MacAdie <james@macadie.co.uk>
2022-01-08 13:10:42 +0000

    Sort based on most common letters
    Now any print call will sort the valid list first to show most common
    letters available in the remaining valid set

--- main.py
+++ main.py
@@ -1,17 +1,21 @@
 import re
 from typing import Counter, List, Tuple
 from itertools import count
+from functools import cmp_to_key
 
 class Guesser:
 
     def __init__(self) -> None:
         self.valid = [] # type: List[str]
+        self.freqs = Counter('abccd')
+        self.known = [] # type: List[str]
         with open('5_letter_words.wl', encoding='UTF-8') as file:
             while line := file.readline():
                 line = line.replace('\n', '').strip()
                 self.valid.append(line)
 
     def add(self, guess: str, mask: Tuple[int, int, int, int, int]) -> None:
+        self.known = [] # type: List[str]
         guess_c = Counter(guess)
         for char in guess_c:
             temp = zip(guess, mask, count(0))
@@ -22,8 +26,10 @@ class Guesser:
             for m, i in char_list:
                 if m == 1:
                     loc.append(i)
+                    self.known.append(char)
                 elif m == 2:
                     exact.append(i)
+                    self.known.append(char)
                 elif m == 0:
                     at_least = False
             if not exact and not loc:
@@ -39,15 +45,10 @@ class Guesser:
 
     def filter_exact(self, char: str, indexes: List[int]) -> None:
         for index in indexes:
-            if index > 0:
-                start = '^.{' + str(index) + '}'
-            else:
-                start = '^'
-            if index < 4:
-                end = '.{' + str(4 - index) + '}$'
-            else:
-                end = '$'
-            pat = re.compile(start + char + end)
+            start = '^' if index == 0 else '^.{' + str(index) + '}'
+            end = '$' if index == 4 else '.{' + str(4 - index) + '}$'
+            pat_str = start + char + end
+            pat = re.compile(pat_str)
             self.valid = [word for word in self.valid if pat.match(word)]
 
     def filter_loc(
@@ -65,13 +66,35 @@ class Guesser:
         self.valid = [word for word in self.valid if pat.match(word)]
         self.filter_exact('[^' + char + ']', indexes)
 
+    def sort_valid(self) -> None:
+        self.freqs = Counter(''.join(self.valid))
+        self.valid = sorted(self.valid, key=cmp_to_key(self.compare))
+
+    def compare(self, item1: str, item2: str) -> int:
+        return self.word_value(item2) - self.word_value(item1)
+
+    def word_value(self, word: str) -> int:
+        output = 0
+        seen = []
+        for char in word:
+            if char in seen and char not in self.known:
+                continue
+            seen.append(char)
+            output += self.freqs[char]
+            if char in self.known:
+                output -= len([x for x in self.known if x == char]) * len(self.valid)
+        return output
+
     def print(self) -> None:
+        self.sort_valid()
         num = len(self.valid)
         print(num)
         for i in range(min(num, 50)):
             print(self.valid[i])
 
 G = Guesser()
-G.add('eagle', (1,0,2,0,0))
-G.add('begin', (0,1,2,1,0))
+G.add('arose', (1,2,0,0,0))
+G.add('train', (0,2,2,0,1))
+G.add('drank', (0,2,2,2,2))
+#G.add('canal', (0,2,2,2,2))
 G.print()
